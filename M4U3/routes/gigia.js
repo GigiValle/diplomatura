c7cc97a045c3da44cfbce2025253c7b1f0ede116
Gigi Valle <gigivalle91@gmail.com>
2022-03-04 11:27:49 -0300

    correción

--- M4U3/routes/gigia.js
+++ M4U3/routes/gigia.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('GIGI A');
+  res.render('gigia');
 });
 
 module.exports = router;
--- M4U3/routes/gigib.js
+++ M4U3/routes/gigib.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('GIGI B');
+  res.render('gigib');
 });
 
 module.exports = router;
