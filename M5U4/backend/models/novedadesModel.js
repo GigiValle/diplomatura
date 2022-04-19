346b4c00e8aaaac069dbcfc1d43626f7009343d1
Gigi Valle <gigivalle91@gmail.com>
2022-04-19 09:32:40 -0300

    modificar y eliminar

--- M5U4/backend/models/novedadesModel.js
+++ M5U4/backend/models/novedadesModel.js
@@ -22,5 +22,35 @@ async function getNovedades()
         }
     }
 
+    async function deleteNovedadById(id) 
+    {
+            var query = 'delete from novedades where id = ?';
+            var rows = await pool.query(query, [id]);
+            return rows;
+        
+    }
+    
+    
+    async function getNovedadById(id)
+    {
+            var query = 'select * from novedades here id = ?';
+            var rows = await pool.query(query, [id]);
+            return rows[0];
+        
+    }
 
-    module.exports = {getNovedades, insertNovedad}
+
+
+    async function modificarNovedadById(obj, id)
+    {
+        try {
+            var query = 'update novedades set ? where id =?';
+            var rows = await pool.query(query, [obj, id]);
+            return rows;
+        } catch (error) {
+            console.log(error);
+            throw error;
+        }
+    }
+
+    module.exports = {getNovedades, insertNovedad, deleteNovedadById, getNovedadById, modificarNovedadById}
