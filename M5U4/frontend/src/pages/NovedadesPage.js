a31106d35e0c822f45306f72c6592cc39d525ed0
Gigi Valle <gigivalle91@gmail.com>
2022-04-27 20:04:33 -0300

    conexión con react

--- M5U4/frontend/src/pages/NovedadesPage.js
+++ M5U4/frontend/src/pages/NovedadesPage.js
@@ -3,7 +3,6 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import NovedadItem from '../components/layout/novedades/NovedadItem';
 
-import React from 'react';
 import '../../src/styles/components/pages/NovedadesPage.css';
 
 
